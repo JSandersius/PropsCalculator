2e06fea60fa12e63a55b0a44afe112156dba3b62
Julian Sandersius <sandersiusjulian@gmail.com>
2018-06-12 09:54:53 -0700

    created Math component

---
+++ propcalc/src/components/Math.js
@@ -0,0 +1,9 @@
+//this is going to be a react component, so we need to rememeber to import react from react
+//create our component variable and export it 
+
+import react from "react";
+
+const Math = () => (); 
+
+
+export default Math; 
