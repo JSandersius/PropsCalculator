70841d21a870d259b3ba8ef0366cce64c969b610
Julian Sandersius <sandersiusjulian@gmail.com>
2018-06-12 09:30:37 -0700

    deleted Math.js and added notes

--- propcalc/src/App.js
+++ propcalc/src/App.js
@@ -1,6 +1,13 @@
+//all this is going to render is the calculator component 
+//easy to work with 
+//now see Calculator.js to see whayt this contains 
+
+
 import React from "react";
 import Calculator from "./components/Calculator";
 
 const App = () => <Calculator />;
 
 export default App;
+
+
