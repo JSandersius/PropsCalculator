21c06895fa92ac91f385def206d82ccd897ef266
Julian Sandersius <sandersiusjulian@gmail.com>
2018-06-12 14:13:20 -0700

    now it works, now to work on logic

--- propcalc/src/components/Math.js
+++ propcalc/src/components/Math.js
@@ -5,9 +5,10 @@ import react from "react";
 //lets make sure that Math accepts props 
 //before anything lets retunr a span with all the props that are being 
 	//passed in to ensure everything is working properly 
-const Math = (props) => (
-	<span>{props.num1} {props.operator} {props.num2}</span>;
-	); 
+const Math = (props) => {
+
+	return <span>{props.num1} {props.operator} {props.num2}</span>;
+	} 
 
 
 export default Math; 
