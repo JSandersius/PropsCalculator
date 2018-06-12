86da102a571ac5f07f4571b4d2558883597645da
Julian Sandersius <sandersiusjulian@gmail.com>
2018-06-12 14:07:10 -0700

    added three props to Math compnenent

--- propcalc/src/components/Calculator.js
+++ propcalc/src/components/Calculator.js
@@ -11,19 +11,21 @@ import Math from "./Math";
 const Calculator = () => (
   // now need to replace each ? with the math component
     //after this we need to pass three props to the math compenent
+      //we give the props the names num1, operator and num 2
+        // when doing this we must remember to wrap the numbers in brackets to let the compiler know that the enclosed coe is javasctipt
 
   <div>
     <p>
-      19 + 341 = <Math /> 
+      19 + 341 = <Math num1={19} operator="+" num2={142} /> 
     </p>
     <p>
-      42 - 17 = <Math />
+      42 - 17 = <Math num1={42} operator="-" num2={17}/>
     </p>
     <p>
-      100 * 3 = <Math />
+      100 * 3 = <Math num1={100} operator="*" num2={3}/>
     </p>
     <p>
-      96 / 4 = <Math />
+      96 / 4 = <Math num1={96} operator="/" num2={4}/>
     </p>
   </div>
 );
